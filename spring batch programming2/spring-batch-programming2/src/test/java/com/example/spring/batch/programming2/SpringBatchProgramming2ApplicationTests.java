1975910a963d10b88fa37457ce71f9e09d5efff9
DANYAL-HAMZA <102705557+DANYAL-HAMZA@users.noreply.github.com>
2024-04-23 09:44:06 +0100

    Add files via upload

---
+++ spring batch programming2/spring-batch-programming2/src/test/java/com/example/spring/batch/programming2/SpringBatchProgramming2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.batch.programming2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchProgramming2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
